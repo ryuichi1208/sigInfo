e1064d7d085c4a139e83ae772db61e78ad2de4b7
ryuichi1208 <ryucrosskey@gmail.com>
2020-06-22 22:45:25 +0900

    fix requirements.txt

--- files/signal_ops.c
+++ files/signal_ops.c
@@ -2,38 +2,76 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <errno.h>
+
+#define unsigne long long u64
+#define SLEEP_TIME 1
+
+unsigned int SIGNAL_ARR[] = {
+    SIGHUP,
+    SIGINT,
+    SIGQUIT,
+    SIGILL,
+    SIGTRAP,
+    SIGABRT,
+    SIGBUS,
+    SIGFPE,
+    SIGKILL,
+    SIGUSR1,
+    SIGSEGV,
+    SIGUSR2,
+    SIGPIPE,
+    SIGALRM,
+    SIGTERM,
+    SIGSTKFLT,
+    SIGCHLD,
+    SIGCONT,
+    SIGSTOP,
+    SIGTSTP,
+    SIGTTIN,
+    SIGTTOU,
+    SIGURG,
+    SIGXCPU,
+    SIGXFSZ,
+    SIGVTALRM,
+    SIGPROF,
+    SIGWINCH,
+    SIGIO,
+    SIGPWR,
+    SIGSYS,
+};
+
+void print_proc_info()
+{
+    pid_t c_pid = getpid();
+    pid_t p_pid = getppid();
+
+    fprintf(stdout, "%d %d\n", c_pid, p_pid);
+}
+
+void signal_mask(sigset_t *set)
+{
+    // シグナル集合を空にし、SIGINT 追加
+    sigemptyset(set);
+
+    for (int i = 0; i < sizeof(SIGNAL_ARR)/sizeof(*SIGNAL_ARR); i++) {
+        if (sigaddset(set, *(SIGNAL_ARR + i)))
+            fprintf(stderr, "[FAILED] Signal = %d  errno =%d", *(SIGNAL_ARR + 1), errno);
+    }
+    sigprocmask(SIG_BLOCK, set, NULL);
+
+}
 
 int main(void)
 {
-    unsigned int sec = 10;
     sigset_t set;
-    pid_t c_pid = getpid();
-
-    fprintf(stdout, "%d\n", c_pid);
-
-    // シグナル集合を空にし、SIGINT 追加
-    //sigemptyset(&set);
-    //sigaddset(&set, SIGINT);
-    //sigaddset(&set, SIGHUP);
-    //sigaddset(&set, SIGQUIT);
-    //sigaddset(&set, SIGILL);
-    //sigaddset(&set, SIGTRAP);
-    //sigaddset(&set, SIGABRT);
-    //sigaddset(&set, SIGBUS);
-    //sigaddset(&set, SIGFPE);
-    //sigaddset(&set, SIGUSR1);
-
-    // SIGINT をブロック (保留) する
-    sigprocmask(SIG_BLOCK, &set, NULL);
-    printf("Block the SIGINT for %d sec\n", sec);
-
-    sleep(sec);
+    signal_mask(&set);
+    sleep(SLEEP_TIME);
 
     // SIGINT のブロックを解除する
-    printf("\nPassed %d sec, unblock the SIGINT\n", sec);
     sigprocmask(SIG_UNBLOCK, &set, NULL);
 
-    printf("Done !\n");
+    fprintf(stdout, "Done !\n");
 
     return 0;
 }
